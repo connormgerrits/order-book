30bab6f5f9040317df3a40f48b9e5f5278ae621c
Connor Michael Gerrits <connormgerrits@connors-air.worcester.local>
2019-10-19 13:31:08 -0400

    added a for-loop with various if-else statements to satisfy 7 of the 8 tests, i plan to refactor later

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,48 @@
 function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+  let updatedBook = []
+
+  if (existingBook == undefined || existingBook.length == 0){
+    updatedBook = updatedBook.concat(incomingOrder)
+    return updatedBook
+  } 
+
+  // i know, this part's hella ugly. i plan to refactor this whole block of code to utilize map, reduce and filter where appropriate
+  for (var i = 0; i < existingBook.length; i++) {
+    if (existingBook[i].price == incomingOrder.price && existingBook[i].quantity == incomingOrder.quantity) {
+
+      if ((existingBook[i].type == 'buy' && incomingOrder.type == 'sell') || (existingBook[i].type == 'sell' && incomingOrder.type == 'buy')) {
+        continue
+      }
+      else {
+        incomingOrder.quantity = existingBook[i].quantity + incomingOrder.quantity
+        updatedBook = updatedBook.concat(incomingOrder)
+        continue
+      }
+    }
+
+    else if (existingBook[i].price == incomingOrder.price && existingBook[i].quantity < incomingOrder.quantity) {
+      if ((existingBook[i].type == 'buy' && incomingOrder.type == 'sell') || (existingBook[i].type == 'sell' && incomingOrder.type == 'buy')) {
+        incomingOrder.quantity = incomingOrder.quantity - existingBook[i].quantity
+        updatedBook = updatedBook.concat(incomingOrder)
+      }
+    }
+
+    else if (existingBook[i].price == incomingOrder.price && existingBook[i].quantity > incomingOrder.quantity) {
+      if ((existingBook[i].type == 'buy' && incomingOrder.type == 'sell') || (existingBook[i].type == 'sell' && incomingOrder.type == 'buy')) {
+        incomingOrder.quantity = existingBook[i].quantity - incomingOrder.quantity
+        incomingOrder.type = existingBook[i].type
+        updatedBook = updatedBook.concat(incomingOrder)
+      }
+    }
+
+    else { 
+      updatedBook = updatedBook.concat(existingBook[i])
+      updatedBook = updatedBook.concat(incomingOrder)
+    }
+  }
+
+  console.log(updatedBook)
+  return updatedBook
 }
 
 module.exports = reconcileOrder
